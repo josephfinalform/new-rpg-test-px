a14155dbe644b27d4b6245ca4902e14ad25b0713
josephfinalform <josephfinalform@users.noreply.github.com>
2026-08-03 00:15:30 +0300

    feat: goblin elite düşmanı - güçlü, hızlı, 20 XP ödüllü

---
+++ aarpg/Enemies/goblin_elite.gd
@@ -0,0 +1,12 @@
+class_name GoblinElite
+extends Goblin
+
+
+func _ready() -> void:
+	max_health = 12
+	move_speed = 65.0
+	damage = 3
+	xp_reward = 20
+	attack_range = 26.0
+	attack_cooldown_time = 0.5
+	super()
